418ee9570fa9d76f2129c6555249c39470cf5127
LoveSy <shana@zju.edu.cn>
2021-02-01 17:59:12 +0800

    v0.5.2.0

---
+++ README.md
@@ -0,0 +1,60 @@
+# LSPosed Framework
+
+![Core](https://github.com/LSPosed/LSPosed/workflows/Core/badge.svg) ![Manager](https://github.com/LSPosed/LSPosed/workflows/Manager/badge.svg)
+
+## Introduction 
+
+A Riru module trying to provide an ART hooking framework (initially for Android Pie) which delivers consistent APIs with the OG Xposed, leveraging YAHFA and SandHook hooking framework, supports Android 8.0 ~ **11**.
+
+> Xposed is a framework for modules that can change the behavior of the system and apps without touching any APKs. That's great because it means that modules can work for different versions and even ROMs without any changes (as long as the original code was not changed too much). It's also easy to undo. As all changes are done in the memory, you just need to deactivate the module and reboot to get your original system back. There are many other advantages, but here is just one more: Multiple modules can do changes to the same part of the system or app. With modified APKs, you to decide for one. No way to combine them, unless the author builds multiple APKs with different combinations.
+
+## Supported Versions
+
+Android 8.1+
+
+## Install
+
+1. Install Magisk v21+
+2. Install [Riru](https://github.com/RikkaApps/Riru/releases) v23+ from Magisk repo.
+3. [Download](#download) and install LSPosed in Magisk Manager
+4. Install LSPosed Manager
+4. Reboot.
+5. Have fun! :)
+
+## Download
+
+You can download LSPosed framework and LSPosed Manager from Github Action or Github Release
+## Useful Links
+
+- [List of Xposed Modules For Android Pie Working With LSPosed](https://forum.xda-developers.com/xposed/list-xposed-modules-android-pie-ed-t3892768) (thanks to Uraniam9 @ xda-developers)
+
+## Get Help
+
+- GitHub issues: [Issues](https://github.com/LSPosed/LSPosed/issues/)
+
+## For Developers
+
+Developers are welcomed to write Xposed modules with hooks based on LSPosed Framework. Module written based on LSPosed framework is fully compatible with the original Xposed Framework, so contrary a Xposed Framework-based module will work well with the LSPosed framework too.
+
+- [Xposed Framework API](https://api.xposed.info/)
+
+We use the module repository of the original Xposed, so you simply upload the module to repository, then you can download your module in LSPosed.
+
+- [Xposed Module Repository](https://repo.xposed.info/)
+
+## Community Discussion
+
+- Telegram: [@LSPosed](http://t.me/LSPosed)
+
+Notice: These community group don't accept any bug report, please use [Get help](#get-help) to report.
+
+## Credits 
+
+- [YAHFA](https://github.com/rk700/YAHFA): the core ART hooking framework
+- [Magisk](https://github.com/topjohnwu/Magisk/): makes all these possible
+- [Riru](https://github.com/RikkaApps/Riru): provides a way to inject codes into zygote process
+- [XposedBridge](https://github.com/rovo89/XposedBridge): the OG xposed framework APIs
+- [dexmaker](https://github.com/linkedin/dexmaker) and [dalvikdx](https://github.com/JakeWharton/dalvik-dx): to dynamiclly generate YAHFA hooker classes
+- [SandHook](https://github.com/ganyao114/SandHook/): ART hooking framework for SandHook variant
+- [Dobby](https://github.com/jmpews/Dobby): used for inline hooking
+- [EdXposed](https://github.com/ElderDrivers/EdXposed): fork source
---
+++ post-fs-data.sh
@@ -0,0 +1,143 @@
+#!/system/bin/sh
+
+grep_prop() {
+    local REGEX="s/^$1=//p"
+    shift
+    local FILES="$@"
+    [[ -z "$FILES" ]] && FILES='/system/build.prop'
+    sed -n "$REGEX" ${FILES} 2>/dev/null | head -n 1
+}
+
+MODDIR=${0%/*}
+
+RIRU_PATH="/data/adb/riru"
+RIRU_PROP="$(magisk --path)/.magisk/modules/riru-core/module.prop"
+TARGET="${RIRU_PATH}/modules"
+
+EDXP_VERSION=$(grep_prop version "${MODDIR}/module.prop")
+EDXP_APICODE=$(grep_prop api "${MODDIR}/module.prop")
+
+ANDROID_SDK=$(getprop ro.build.version.sdk)
+BUILD_DESC=$(getprop ro.build.description)
+PRODUCT=$(getprop ro.build.product)
+MODEL=$(getprop ro.product.model)
+MANUFACTURER=$(getprop ro.product.manufacturer)
+BRAND=$(getprop ro.product.brand)
+FINGERPRINT=$(getprop ro.build.fingerprint)
+ARCH=$(getprop ro.product.cpu.abi)
+DEVICE=$(getprop ro.product.device)
+ANDROID=$(getprop ro.build.version.release)
+BUILD=$(getprop ro.build.id)
+
+RIRU_VERSION=$(grep_prop version $RIRU_PROP)
+RIRU_VERCODE=$(grep_prop versionCode $RIRU_PROP)
+RIRU_APICODE=$(cat "${RIRU_PATH}/api_version")
+
+MAGISK_VERSION=$(magisk -v)
+MAGISK_VERCODE=$(magisk -V)
+
+livePatch() {
+    # Should be deprecated now. This is for debug only.
+    supolicy --live "allow system_server system_server process execmem" \
+                    "allow system_server system_server memprotect mmap_zero"
+}
+
+MISC_PATH=$(cat /data/adb/lspd/misc_path)
+BASE_PATH="/data/misc/$MISC_PATH"
+
+LOG_PATH="${BASE_PATH}/0/log"
+CONF_PATH="${BASE_PATH}/0/conf"
+DISABLE_VERBOSE_LOG_FILE="${CONF_PATH}/disable_verbose_log"
+LOG_VERBOSE=true
+OLD_PATH=${PATH}
+PATH=${PATH#*:}
+PATH_INFO=$(ls -ldZ "${BASE_PATH}")
+PATH=${OLD_PATH}
+PATH_OWNER=$(echo "${PATH_INFO}" | awk -F " " '{print $3":"$4}')
+PATH_CONTEXT=$(echo "${PATH_INFO}" | awk -F " " '{print $5}')
+
+if [[ -f ${DISABLE_VERBOSE_LOG_FILE} ]]; then
+    LOG_VERBOSE=false
+fi
+
+# If logcat client is kicked out by klogd server, we'll restart it.
+# However, if it is killed manually or by LSPosed Manager, we'll exit.
+# Refer to https://github.com/ElderDrivers/LSPosed/pull/575 for more information.
+loop_logcat() {
+    while true
+    do
+        logcat $*
+        if [[ $? -ne 1 ]]; then
+            break
+        fi
+    done
+}
+
+start_log_cather () {
+    LOG_FILE_NAME=$1
+    LOG_TAG_FILTERS=$2
+    CLEAN_OLD=$3
+    START_NEW=$4
+    LOG_FILE="${LOG_PATH}/${LOG_FILE_NAME}.log"
+    PID_FILE="${LOG_PATH}/${LOG_FILE_NAME}.pid"
+    mkdir -p ${LOG_PATH}
+    if [[ ${CLEAN_OLD} == true ]]; then
+        rm "${LOG_FILE}.old"
+        mv "${LOG_FILE}" "${LOG_FILE}.old"
+    fi
+    rm "${LOG_PATH}/${LOG_FILE_NAME}.pid"
+    if [[ ${START_NEW} == false ]]; then
+        return
+    fi
+    touch "${LOG_FILE}"
+    touch "${PID_FILE}"
+    echo "LSPosed Log">>"${LOG_FILE}"
+    echo "--------- beginning of information">>"${LOG_FILE}"
+    echo "Manufacturer: ${MANUFACTURER}">>"${LOG_FILE}"
+    echo "Brand: ${BRAND}">>"${LOG_FILE}"
+    echo "Device: ${DEVICE}">>"${LOG_FILE}"
+    echo "Product: ${PRODUCT}">>"${LOG_FILE}"
+    echo "Model: ${MODEL}">>"${LOG_FILE}"
+    echo "Fingerprint: ${FINGERPRINT}">>"${LOG_FILE}"
+    echo "ROM description: ${BUILD_DESC}">>"${LOG_FILE}"
+    echo "Architecture: ${ARCH}">>"${LOG_FILE}"
+    echo "Android build: ${BUILD}">>"${LOG_FILE}"
+    echo "Android version: ${ANDROID}">>"${LOG_FILE}"
+    echo "Android sdk: ${ANDROID_SDK}">>"${LOG_FILE}"
+    echo "LSPosed version: ${EDXP_VERSION}">>"${LOG_FILE}"
+    echo "LSPosed api: ${EDXP_APICODE}">>"${LOG_FILE}"
+    echo "Riru version: ${RIRU_VERSION} (${RIRU_VERCODE})">>"${LOG_FILE}"
+    echo "Riru api: ${RIRU_APICODE}">>"${LOG_FILE}"
+    echo "Magisk: ${MAGISK_VERSION%:*} (${MAGISK_VERCODE})">>"${LOG_FILE}"
+    loop_logcat -f "${LOG_FILE}" *:S "${LOG_TAG_FILTERS}" &
+    LOG_PID=$!
+    echo "${LOG_PID}">"${LOG_PATH}/${LOG_FILE_NAME}.pid"
+}
+
+# execute live patch if rule not found
+[[ -f "${MODDIR}/sepolicy.rule" ]] || livePatch
+
+# start_verbose_log_catcher
+start_log_cather all "LSPosed:V XSharedPreferences:V LSPosed-Bridge:V LSPosedManager:V *:F" true ${LOG_VERBOSE}
+
+# start_bridge_log_catcher
+start_log_cather error "XSharedPreferences:V LSPosed-Bridge:V" true true
+
+if [[ -f "/data/adb/riru/modules/lspd.prop" ]]; then
+    CONFIG=$(cat "/data/adb/riru/modules/lspd.prop")
+    [[ -d "${TARGET}/${CONFIG}" ]] || mkdir -p "${TARGET}/${CONFIG}"
+    cp "${MODDIR}/module.prop" "${TARGET}/${CONFIG}/module.prop"
+fi
+
+chcon -R u:object_r:system_file:s0 "${MODDIR}"
+chcon -R ${PATH_CONTEXT} "${LOG_PATH}"
+chown -R ${PATH_OWNER} "${LOG_PATH}"
+chmod -R 666 "${LOG_PATH}"
+
+if [[ ! -z "${MISC_PATH}" ]]; then
+    mkdir -p "${BASE_PATH}/cache"
+    chcon -R u:object_r:magisk_file:s0 "${BASE_PATH}"
+    chmod 771 "${BASE_PATH}"
+    chmod 777 "${BASE_PATH}/cache"
+fi
+rm -f /data/adb/lspd/new_install
